387502715c0732c022c51879767f664f0ea330a8
ManishV5 <87130329+ManishV5@users.noreply.github.com>
2022-02-22 16:20:17 +0000

    create basic architecture

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask, flash, render_template, request
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
